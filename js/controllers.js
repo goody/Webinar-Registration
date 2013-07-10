5f3bb02e8b68c44fa016f16268fccd38d57ba4f2
Scot Goodhart <scot.goodhart@gmail.com>
2013-07-10 17:42:59 -0500

    Setting up with test data

--- js/controllers.js
+++ js/controllers.js
@@ -3,9 +3,15 @@
 /* Controllers */
 
 angular.module('webinarRegApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+  controller('WebinarsController', [function($scope) {
+  	$scope.webinar = { "title": "Selling to the Seller",
+  	"date": "March 1st",
+  	"presenter": "John Johnson, CEO, Johnson Co."}
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('AttendeesController', [function($scope) {
+  	$scope.attnedee = {	"name" : "Johnny Tester",
+  		"company" : "Test Co"  },
+  	{	"name": "Cindy Practice",
+  		"company" : "Practice Co"}
 
   }]);
