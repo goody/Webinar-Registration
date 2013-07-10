73e7d4f1ec42b83461b11ec35489317ce0f91b2d
Scot Goodhart <scot.goodhart@gmail.com>
2013-07-10 16:04:19 -0500

    Init Commit
    Angular-seed to begin webinar registration app

---
+++ js/controllers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('webinarRegApp.controllers', []).
+  controller('MyCtrl1', [function() {
+
+  }])
+  .controller('MyCtrl2', [function() {
+
+  }]);
