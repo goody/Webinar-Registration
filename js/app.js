e3f575d4d467003b142481ca1fee4aca64a67eb7
Scot Goodhart <scot.goodhart@gmail.com>
2013-07-11 17:59:50 -0500

    Figuring out syntax

--- js/app.js
+++ js/app.js
@@ -4,16 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('webinarRegApp', ['webinarRegApp.filters', 'webinarRegApp.services', 'webinarRegApp.directives', 'webinarRegApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-    	.when('/webinar',
-    		 {
-    		 	templateUrl: 'partials/partial1.html', 
-    		 	controller: 'WebinarsController'
-    		 })
-    	.when('/attendees', 
-    		{
-    			templateUrl: 'partials/partial2.html', 
-    			controller: 'AttendeesController'
-    		})
-    	.otherwise({redirectTo: '/webinar'});
+    $routeProvider.when('/webinar', {templateUrl: 'partials/partial1.html', controller: 'WebinarsController'});
+    $routeProvider.when('/attendees', {templateUrl: 'partials/partial2.html', controller: 'AttendeesController'});
+    $routeProvider.otherwise({redirectTo: '/view1'});
   }]);
--- js/controllers.js
+++ js/controllers.js
@@ -2,19 +2,40 @@
 
 /* Controllers */
 
-var appControllers = {};
+// var appControllers = {};
 
-appControllers.WebinarsController = function($scope){
-	$scope.webinar =[
-		{"title": "Selling to the Seller", "date": "March 1st","presenter": "John Johnson, CEO, Johnson Co."}
-  	];
-};
+// appControllers.WebinarsController = function($scope, webinarFactory){
+//   	$scope.webinar = [
+// 		{"title": "Selling to the Seller", "date": "March 1st","presenter": "John Johnson, CEO, Johnson Co."}
+//   	];
+// };
 
-appControllers.AttendeesController = function($scope){
-	$scope.attendee = [
-  		{"name" : "Johnny Tester","company" : "Test Co"  },
-  		{"name": "Cindy Practice","company" : "Practice Co"}
-  	];
-};
+// appControllers.AttendeesController = function($scope, attendeesFactory){
+// 	$scope.attendees = attendeesFactory.getAttendees();
+// };
 
-angular.module('webinarRegApp.controllers', []).controller(appControllers);
+
+// webinarRegApp.controller('WebinarsController', function($scope, webinarFactory){
+// 	$scope.webinar = getWebinar();
+// });
+
+angular.module('webinarRegApp.controllers', [])
+	.controller('WebinarsController', function($scope, webinarFactory){
+		// $scope.webinar = webinarFactory.getWebinar();
+		console.log("WebinarsController");
+		$scope.webinar = getWebinar();
+		// $scope.webinar = {	title: "Selling to the Seller", 
+		// 			date: "March 1st",
+		// 			presenter: "John Johnson, CEO, Johnson Co."
+		// 		};
+		$scope.what = "Huh?!";
+	})
+	.controller('AttendeesController', function($scope, attendeesFactory){
+		$scope.attendees = attendeesFactory.getAttendees();
+		console.log("FUCK THIS IS HARD");
+	})
+	.controller('PageController', function($scope){
+		$scope.page = [
+			{"title": "THE PAGE TITLE"}
+		];
+	});
