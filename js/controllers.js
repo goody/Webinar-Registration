14fc505d6ccff36e906ca690286e18bbef6bb060
Scot Goodhart <scot.goodhart@gmail.com>
2013-07-16 17:30:47 -0500

    fixed syntax

--- js/controllers.js
+++ js/controllers.js
@@ -20,22 +20,20 @@
 // });
 
 angular.module('webinarRegApp.controllers', [])
-	.controller('WebinarsController', function($scope, webinarFactory){
+	.controller('WebinarsController', function($scope, WebinarFactory){
 		// $scope.webinar = webinarFactory.getWebinar();
-		console.log("WebinarsController");
-		$scope.webinar = getWebinar();
+		//console.log("WebinarsController");
+		$scope.webinar = WebinarFactory;
 		// $scope.webinar = {	title: "Selling to the Seller", 
 		// 			date: "March 1st",
 		// 			presenter: "John Johnson, CEO, Johnson Co."
 		// 		};
-		$scope.what = "Huh?!";
+		$scope.what = {what:"Huh?!"};
 	})
-	.controller('AttendeesController', function($scope, attendeesFactory){
-		$scope.attendees = attendeesFactory.getAttendees();
+	.controller('AttendeesController', function($scope, AttendeesFactory){
+		$scope.attendees = AttendeesFactory;
 		console.log("FUCK THIS IS HARD");
 	})
 	.controller('PageController', function($scope){
-		$scope.page = [
-			{"title": "THE PAGE TITLE"}
-		];
+		$scope.page = {title: "THE PAGE TITLE"};
 	});
