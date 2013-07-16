4c67f795a445e349c62f5dfbd89a8ac7df39c087
Scot Goodhart <scot.goodhart@gmail.com>
2013-07-16 17:30:47 -0500

    fixed syntax

--- js/services.js
+++ js/services.js
@@ -7,26 +7,28 @@
 // In this case it is a simple value service.
 angular.module('webinarRegApp.services', [])
   .value('version', '0.2')
-  .factory('webinarFactory', function(){
+  .factory('WebinarFactory', function(){
   	var factory = {};
   	var webinar = { 
-  		"title": "Selling to the Seller", "date": "March 1st","presenter": "John Johnson, CEO, Johnson Co." 
+  		title: "Selling to the Seller", 
+      date: "March 1st",
+      presenter: "John Johnson, CEO, Johnson Co." 
   	};
-  	factory.getWebinar = function(){
+  	//factory.getWebinar = function(){
   		return webinar;
-  	};
-  	factory.return;
+  	// };
+  	// factory.return;
 
   })
-  .factory('attendeesFactory', function(){
+  .factory('AttendeesFactory', function(){
   	var factory = {};
   	var attendees = [
   		{"name" : "Johnny Tester","company" : "Test Co"  },
   		{"name": "Cindy Practice","company" : "Practice Co"}
   	];
-  	factory.getAttendees = function(){
+  	//factory.getAttendees = function(){
   		return attendees;
-  	};
-  	factory.return;
+  	// };
+  	// factory.return;
   });
 
